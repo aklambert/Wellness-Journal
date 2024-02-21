218d8d534bdbe083dca8ae870bf95f019fcc1d14
aklambert <alanaks558@gmail.com>
2024-02-21 10:10:21 -0600

    #5, UI for welcome screen

---
+++ app/src/main/java/com/example/wellnessjournal/WelcomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.wellnessjournal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+
+    }
+}
