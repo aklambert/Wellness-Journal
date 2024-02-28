ed186e7ff6c47ae8b021e9746bb81b8721ee6b82
aklambert <alanaks558@gmail.com>
2024-02-28 10:41:53 -0600

    #11, Set up timer page

--- app/src/main/java/com/example/wellnessjournal/ui/timer/TimerFragment.kt
+++ app/src/main/java/com/example/wellnessjournal/ui/timer/TimerFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.wellnessjournal.databinding.FragmentTimerBinding
@@ -22,8 +21,7 @@ class TimerFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val timerViewModel =
-            ViewModelProvider(this).get(TimerViewModel::class.java)
+        val timerViewModel = ViewModelProvider(this).get(TimerViewModel::class.java)
 
         _binding = FragmentTimerBinding.inflate(inflater, container, false)
         val root: View = binding.root
