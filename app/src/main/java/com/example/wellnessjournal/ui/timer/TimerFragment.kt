a8f827070987eebc672658c8142d318cec5848ec
aklambert <alanaks558@gmail.com>
2024-03-07 11:24:52 -0600

    #11, Countdown timer sets countdown based on user input

--- app/src/main/java/com/example/wellnessjournal/ui/timer/TimerFragment.kt
+++ app/src/main/java/com/example/wellnessjournal/ui/timer/TimerFragment.kt
@@ -3,6 +3,7 @@ package com.example.wellnessjournal.ui.timer
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,6 +23,7 @@ class TimerFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var timer: CountDownTimer
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,12 +42,11 @@ class TimerFragment : Fragment() {
 
         val btnTimer: Button = root.findViewById(R.id.btn_timer)
 
+
         // Start a timer when the START TIMER button is clicked
         btnTimer.setOnClickListener{
-            timer(root, btnTimer)
+            toggleTimer(root, btnTimer)
         }
-
-
 
         return root
     }
@@ -53,16 +54,63 @@ class TimerFragment : Fragment() {
     /**
      * Starts or stops the timer, depending on if the timer is running or not
      */
-    fun timer(root: View, timer_btn: Button) {
+    fun toggleTimer(root: View, timer_btn: Button) {
         // Get view items
-        val editTxtHours: EditText = root.findViewById(R.id.input_hours)
-        val editTxtMinutes: EditText = root.findViewById(R.id.input_minutes)
+        val editTextHours: EditText = root.findViewById(R.id.input_hours)
+        val editTextMinutes: EditText = root.findViewById(R.id.input_minutes)
         val editTextSeconds: EditText = root.findViewById(R.id.input_seconds)
+
         val colon1: TextView = root.findViewById(R.id.colon1)
         val colon2: TextView = root.findViewById(R.id.colon2)
+        val timerProgressView: TextView = root.findViewById(R.id.timer_progress_view)
 
-        val hours: String = editTxtHours.toString()
-        val minutes: String = editTxtMinutes.toString()
+        // Default timer values set to 0 hours, 0 minutes, 0 seconds
+        var msHrs: Long = 0
+        var msMin: Long = 0
+        var msSec: Long = 0
+
+        if (editTextHours.text.toString() !== "") {
+
+            val hrsLong: Long? = editTextHours.text.toString().toLongOrNull()
+            if (hrsLong != null) {
+                msHrs = hrsLong * 3600000
+            }
+        }
+
+        if (editTextMinutes.text.toString() !== "") {
+            val minLong: Long? = editTextMinutes.text.toString().toLongOrNull()
+            if (minLong != null) {
+                msMin = minLong * 60000
+            }
+        }
+        if (editTextSeconds.text.toString() !== "") {
+            val secLong: Long? = editTextMinutes.text.toString().toLongOrNull()
+            if (secLong != null) {
+                msSec = secLong * 1000
+            }
+        }
+
+        // Convert input time to one value in milliseconds for countdown timer
+        val msTotal: Long = msHrs + msMin + msSec
+
+        // Initialize countdown timer
+        timer = object : CountDownTimer(msTotal, 1) {
+            override fun onTick(millisUntilFinished: Long) {
+                val hrs: Long = millisUntilFinished / 3600000
+                val timeNoHrs: Long = millisUntilFinished - (hrs * 3600000)
+                val mins: Long = timeNoHrs / 60000
+                val timeNoHrsNoMins: Long = timeNoHrs - (mins * 60000)
+                val seconds: Long = timeNoHrsNoMins / 1000
+
+                val progress: String = hrs.toString() + " : " + mins.toString() +
+                        " : " + seconds.toString()
+                timerProgressView.text = progress
+            }
+
+            override fun onFinish() {
+                timerProgressView.text = "Timer Finished!"
+            }
+        }
 
         // Check shared preferences to see if timer is currently running
         val prefs = this.requireActivity().getSharedPreferences("pref", Context.MODE_PRIVATE)
@@ -71,17 +119,22 @@ class TimerFragment : Fragment() {
         if (prefs.contains("timer_running") && prefs.getBoolean("timer_running", true)) {
 
             // Show the input items when the timer is stopped
-            editTxtHours.visibility = View.VISIBLE
-            editTxtMinutes.visibility = View.VISIBLE
+            editTextHours.visibility = View.VISIBLE
+            editTextMinutes.visibility = View.VISIBLE
             editTextSeconds.visibility = View.VISIBLE
             colon1.visibility = View.VISIBLE
             colon2.visibility = View.VISIBLE
 
-            // Show the button as a START TIMER button
+            // Show the button as a START TIMER button and hide the time remaining
             timer_btn.text = getString(R.string.start_timer)
             timer_btn.text = getString(R.string.start_timer)
             context?.let { ContextCompat.getColor(it, R.color.primary_color) }
                 ?.let { timer_btn.setBackgroundColor(it) }
+
+            timerProgressView.visibility = View.GONE
+            if (timer !== null) {
+                timer.cancel()
+            }
 
             // Set shared preference setting to tell that the timer has been set and is running
             val prefsEditor: SharedPreferences.Editor = prefs.edit()
@@ -93,8 +146,8 @@ class TimerFragment : Fragment() {
         else {
 
             // Do not show the input items when the timer is running
-            editTxtHours.visibility = View.GONE
-            editTxtMinutes.visibility = View.GONE
+            editTextHours.visibility = View.GONE
+            editTextMinutes.visibility = View.GONE
             editTextSeconds.visibility = View.GONE
             colon1.visibility = View.GONE
             colon2.visibility = View.GONE
@@ -103,6 +156,14 @@ class TimerFragment : Fragment() {
             timer_btn.text = getString(R.string.stop_timer)
             context?.let { ContextCompat.getColor(it, R.color.danger) }
                 ?.let { timer_btn.setBackgroundColor(it) }
+
+            // Start the timer
+
+            timer.start()
+
+            // Show time remaining
+            timerProgressView.visibility = View.VISIBLE
+
 
             // Set shared preference setting to tell that the timer has been set and is running
             val prefsEditor: SharedPreferences.Editor = prefs.edit()
