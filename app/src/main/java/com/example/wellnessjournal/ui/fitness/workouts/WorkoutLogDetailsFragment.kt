bd7a616c97b8440b2b219698c5aa48210d3c14ee
aklambert <alanaks558@gmail.com>
2024-04-14 13:41:16 -0500

    #74, Ability to update workout log dates

--- app/src/main/java/com/example/wellnessjournal/ui/fitness/workouts/WorkoutLogDetailsFragment.kt
+++ app/src/main/java/com/example/wellnessjournal/ui/fitness/workouts/WorkoutLogDetailsFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -14,6 +15,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.wellnessjournal.R
 import com.example.wellnessjournal.data.entities.Workout
+import com.example.wellnessjournal.data.entities.WorkoutLog
 import com.example.wellnessjournal.databinding.FragmentWorkoutLogDetailsBinding
 
 class WorkoutLogDetailsFragment : Fragment() {
@@ -30,10 +32,10 @@ class WorkoutLogDetailsFragment : Fragment() {
 
         // ViewModel setup
         val workoutLogVM: WorkoutLogViewModel = ViewModelProvider(this)[WorkoutLogViewModel::class.java]
+        val logDay: TextView = root.findViewById(R.id.workout_log_day)
 
         // Show saved data
-        val logTitle: String = "Workout log " + selectedWorkoutLog.selectedWorkoutLog.workoutLogDate + ":"
-        root.findViewById<TextView>(R.id.name_workout_log).text = logTitle
+        logDay.text = selectedWorkoutLog.selectedWorkoutLog.workoutLogDate
 
         // Get workout to show details
         val workoutLiveData = workoutLogVM.getWorkoutById(selectedWorkoutLog.selectedWorkoutLog.workoutId)
@@ -51,6 +53,15 @@ class WorkoutLogDetailsFragment : Fragment() {
         val deleteBtn: Button = root.findViewById(R.id.btn_delete_workout_log)
         deleteBtn.setOnClickListener {
             workoutLogVM.deleteWorkoutLog(selectedWorkoutLog.selectedWorkoutLog)
+            findNavController().navigate(R.id.navigation_workout_logs)
+        }
+
+        // Listen for when someone saves a workout log date edit
+        val saveBtn: Button = root.findViewById(R.id.btn_update_workoutlog_date)
+        saveBtn.setOnClickListener {
+            val newDate = logDay.text.toString()
+            val log = WorkoutLog(selectedWorkoutLog.selectedWorkoutLog.workoutLogId, selectedWorkoutLog.selectedWorkoutLog.workoutId, newDate)
+            workoutLogVM.updateWorkoutLog(log)
             findNavController().navigate(R.id.navigation_workout_logs)
         }
 
